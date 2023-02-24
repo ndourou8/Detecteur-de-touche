69fb760d5c4a3675895a59ff24e6aeac878dfbce
ndourou8 <saneahmadou98@gmail.com>
2023-02-24 20:00:02 +0100

    Mon detecteur de touche

---
+++ app.js
@@ -0,0 +1,7 @@
+// Selection des element
+const touche = document.querySelector('.key h2');
+const code = document.querySelector('.code h2');
+const div_code = document.querySelector('.code');
+const overlay = document.querySelector('.overlay');
+
+console.log(overlay);
